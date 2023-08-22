6d24e1cbeeccdd91b9c3e85a356bab07061d7cff
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-22 16:54:39 -0400

    9th problem of if/else skills practice

--- if_else_practice.js
+++ if_else_practice.js
@@ -71,15 +71,24 @@
 
 // # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
 
-var x = 99; 
-if (x === 100) {
-  console.log(100); 
-} else if (x === 99) {
-  console.log(99); 
-} else {
-  console.log(0);
-}
+// var x = 99; 
+// if (x === 100) {
+//   console.log(100); 
+// } else if (x === 99) {
+//   console.log(99); 
+// } else {
+//   console.log(0);
+// }
 
 // # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+var x = -1; 
+var y = 25; 
+
+if (x < 0 && y > 0) { 
+  console.log(1);
+} else {
+  console.log(0); 
+}
 
 // # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
