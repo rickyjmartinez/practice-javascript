d159dbc7398aac1cf6d773a469852d8619dbc774
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-22 21:33:25 -0400

    2nd problem of string js practice

--- strings_practice.js
+++ strings_practice.js
@@ -1,5 +1,11 @@
 // # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
-var first = "ricky";
+// var first = "ricky";
+// var last = "martinez"; 
+// console.log(first + " " + last); 
+
+// # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+var first = "ricky"; 
 var last = "martinez"; 
-console.log(first + " " + last); 
+console.log(`${first} ${last}`)
