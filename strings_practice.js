42792d387ad5a8432fafea48cac9ccb62b0cdb42
ricky martinez <rickyjmartinez2@gmail.com>
2023-08-22 21:35:56 -0400

    3rd problem of string js practice

--- strings_practice.js
+++ strings_practice.js
@@ -6,6 +6,13 @@
 
 // # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
-var first = "ricky"; 
-var last = "martinez"; 
-console.log(`${first} ${last}`);
+// var first = "ricky"; 
+// var last = "martinez"; 
+// console.log(`${first} ${last}`);
+
+// # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+var input = "marco"; 
+if (input === "marco") {
+  console.log("polo");
+}
