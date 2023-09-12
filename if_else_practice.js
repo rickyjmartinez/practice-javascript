c407a4a89b2baa5a3542526001b00aab01e70207
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-12 11:35:22 -0400

    more if else js practice

--- if_else_practice.js
+++ if_else_practice.js
@@ -105,3 +105,25 @@
 // } else {
 //   console.log(1); 
 // }
+
+// # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+var x = 10; 
+
+if (x === 10) {
+  console.log(0);
+} else {
+  console.log(-1); 
+}
+
+// # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+let a = 17; 
+
+if (a < 10) {
+  console.log(-1);
+} else if (a > 10) {
+  console.log(1); 
+} else {
+  console.log(0); 
+}
