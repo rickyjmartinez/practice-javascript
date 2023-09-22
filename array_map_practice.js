2f4182216c7f7103795cb6d11a09c5f7f4808067
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-22 10:05:51 -0400

    array map practice questions 5-7

--- array_map_practice.js
+++ array_map_practice.js
@@ -39,14 +39,33 @@ a.forEach(function (num) {
   b.push(num + 7); 
 }); 
 console.log(b); 
+
 // #  5. Start with an array of strings and create a new array with each string's length.
 // #     For example, ["hello", "goodbye"] becomes [5, 7].
+
+var c = ["hello", "goodbye"]; 
+var d = []; 
+c.forEach(function (word) {
+  d.push(word.length); 
+}); 
+console.log(d); 
+
 
 // #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
+var e = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]; 
+var ages = []; 
+
+e.forEach(function (person) { 
+  ages.push(person.age); 
+}); 
+console.log(ages); 
+
 // #  7. Start with an array of numbers and create a new array with each number divided by 2.
 // #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+
+
 
 // #  8. Start with an array of strings and create a new array with each string's first letter only.
 // #     For example, ["hello", "goodbye"] becomes ["h", "g"].
