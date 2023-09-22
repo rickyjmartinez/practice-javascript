6cfd236d893ea9d1fe0f6b4854049e4cb5b09c62
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-22 10:20:39 -0400

    array map practiceproblems 7-10

--- array_map_practice.js
+++ array_map_practice.js
@@ -65,13 +65,48 @@ console.log(ages);
 // #  7. Start with an array of numbers and create a new array with each number divided by 2.
 // #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
 
+var f = [1, 2, 3]; 
+var half = []; 
 
+f.forEach(function (num) {
+  half.push(num / 2); 
+});
+console.log(half); 
 
 // #  8. Start with an array of strings and create a new array with each string's first letter only.
 // #     For example, ["hello", "goodbye"] becomes ["h", "g"].
 
+var g = ["hello", "goodbye"] ; 
+var first = []; 
+
+g.forEach(function (word) {
+  first.push(word[0]); 
+});
+
+console.log(first);
+
+
 // # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
 
+var humans = [
+  { name: "Alice", age: 27 },
+  { name: "Blane", age: 16 },
+];
+var agesDoubled = [];
+humans.forEach(function (person) {
+  agesDoubled.push(person.age * 2);
+});
+console.log(agesDoubled);
+
+
 // # 10. Start with an array of numbers and create a new array with each number converted into a string.
 // #     For example, [1, 2, 3] becomes ["1", "2", "3"].
+
+var numbers2 = [1,2,3] ; 
+var stringNum = []; 
+
+numbers2.forEach(function (num) {
+  stringNum.push(num.toString()); 
+}); 
+console.log(stringNum); 
