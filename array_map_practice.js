5d8f6cb1b7240d83131a7700daf68ed0da5ad1c6
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-25 12:57:05 -0400

    array map js practice

--- array_map_practice.js
+++ array_map_practice.js
@@ -1,112 +1,160 @@
 
+// // #  1. Start with an array of numbers and create a new array with each number times 3.
+// // #     For example, [1, 2, 3] becomes [3, 6, 9].
+// var array = [1,2,3]; 
+// var newArray = array.map(timesThree); 
+
+// function timesThree(array) {
+//   return array * 3; 
+// }
+// console.log(newArray); 
+
+// // #  2. Start with an array of strings and create a new array with each string upcased.
+// // #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+// var words = ["hello", "goodbye"];
+// console.log(words.map(a => a.toUpperCase()));
+
+
+// // #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
+// // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+
+// var people = [
+//   { name: "Alice", age: 27 },
+//   { name: "Blane", age: 16 },
+// ];
+// var names = [];
+// people.forEach(function (person) {
+//   names.push(person.name);
+// });
+// console.log(names);
+
+
+// // #  4. Start with an array of numbers and create a new array with each number plus 7.
+// // #     For example, [1, 2, 3] becomes [8, 9, 10].
+
+// var a = [1, 2, 3];
+// var b = []; 
+// a.forEach(function (num) {
+//   b.push(num + 7); 
+// }); 
+// console.log(b); 
+
+// // #  5. Start with an array of strings and create a new array with each string's length.
+// // #     For example, ["hello", "goodbye"] becomes [5, 7].
+
+// var c = ["hello", "goodbye"]; 
+// var d = []; 
+// c.forEach(function (word) {
+//   d.push(word.length); 
+// }); 
+// console.log(d); 
+
+
+// // #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
+// // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+
+// var e = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]; 
+// var ages = []; 
+
+// e.forEach(function (person) { 
+//   ages.push(person.age); 
+// }); 
+// console.log(ages); 
+
+// // #  7. Start with an array of numbers and create a new array with each number divided by 2.
+// // #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+
+// var f = [1, 2, 3]; 
+// var half = []; 
+
+// f.forEach(function (num) {
+//   half.push(num / 2); 
+// });
+// console.log(half); 
+
+// // #  8. Start with an array of strings and create a new array with each string's first letter only.
+// // #     For example, ["hello", "goodbye"] becomes ["h", "g"].
+
+// var g = ["hello", "goodbye"] ; 
+// var first = []; 
+
+// g.forEach(function (word) {
+//   first.push(word[0]); 
+// });
+
+// console.log(first);
+
+
+// // # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
+// // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
+
+// var humans = [
+//   { name: "Alice", age: 27 },
+//   { name: "Blane", age: 16 },
+// ];
+// var agesDoubled = [];
+// humans.forEach(function (person) {
+//   agesDoubled.push(person.age * 2);
+// });
+// console.log(agesDoubled);
+
+
+// // # 10. Start with an array of numbers and create a new array with each number converted into a string.
+// // #     For example, [1, 2, 3] becomes ["1", "2", "3"].
+
+// var numbers2 = [1,2,3] ; 
+// var stringNum = []; 
+
+// numbers2.forEach(function (num) {
+//   stringNum.push(num.toString()); 
+// }); 
+// console.log(stringNum); 
+
 // #  1. Start with an array of numbers and create a new array with each number times 3.
 // #     For example, [1, 2, 3] becomes [3, 6, 9].
-var array = [1,2,3]; 
-var newArray = array.map(timesThree); 
 
-function timesThree(array) {
-  return array * 3; 
-}
-console.log(newArray); 
+var a = [1, 2, 3]; 
+var times3 = [];
+a.forEach(function(num) {
+  times3.push(num * 3); 
+});
+console.log(times3); 
+
 
 // #  2. Start with an array of strings and create a new array with each string upcased.
 // #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
-var words = ["hello", "goodbye"];
-console.log(words.map(a => a.toUpperCase()));
+var b = ["hello", "goodbye"]; 
+var cap = []; 
 
+b.forEach(function (word) {
+  cap.push(word.toUpperCase()); 
+});
+console.log(cap); 
 
 // #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
 
-var people = [
-  { name: "Alice", age: 27 },
-  { name: "Blane", age: 16 },
-];
-var names = [];
-people.forEach(function (person) {
-  names.push(person.name);
-});
-console.log(names);
 
 
 // #  4. Start with an array of numbers and create a new array with each number plus 7.
 // #     For example, [1, 2, 3] becomes [8, 9, 10].
 
-var a = [1, 2, 3];
-var b = []; 
-a.forEach(function (num) {
-  b.push(num + 7); 
-}); 
-console.log(b); 
-
 // #  5. Start with an array of strings and create a new array with each string's length.
 // #     For example, ["hello", "goodbye"] becomes [5, 7].
-
-var c = ["hello", "goodbye"]; 
-var d = []; 
-c.forEach(function (word) {
-  d.push(word.length); 
-}); 
-console.log(d); 
-
 
 // #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
-var e = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]; 
-var ages = []; 
-
-e.forEach(function (person) { 
-  ages.push(person.age); 
-}); 
-console.log(ages); 
-
 // #  7. Start with an array of numbers and create a new array with each number divided by 2.
 // #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
-
-var f = [1, 2, 3]; 
-var half = []; 
-
-f.forEach(function (num) {
-  half.push(num / 2); 
-});
-console.log(half); 
 
 // #  8. Start with an array of strings and create a new array with each string's first letter only.
 // #     For example, ["hello", "goodbye"] becomes ["h", "g"].
 
-var g = ["hello", "goodbye"] ; 
-var first = []; 
-
-g.forEach(function (word) {
-  first.push(word[0]); 
-});
-
-console.log(first);
-
-
 // # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
 
-var humans = [
-  { name: "Alice", age: 27 },
-  { name: "Blane", age: 16 },
-];
-var agesDoubled = [];
-humans.forEach(function (person) {
-  agesDoubled.push(person.age * 2);
-});
-console.log(agesDoubled);
-
-
 // # 10. Start with an array of numbers and create a new array with each number converted into a string.
 // #     For example, [1, 2, 3] becomes ["1", "2", "3"].
-
-var numbers2 = [1,2,3] ; 
-var stringNum = []; 
-
-numbers2.forEach(function (num) {
-  stringNum.push(num.toString()); 
-}); 
-console.log(stringNum); 
--- methods_javascript_practice.js
+++ methods_javascript_practice.js
@@ -1,57 +1,57 @@
-// # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+// // # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-var inputWord = "word"; 
-console.log(inputWord.toUpperCase());
+// var inputWord = "word"; 
+// console.log(inputWord.toUpperCase());
 
-// # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+// // # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
-var inputNumber = 240;
-if (inputNumber > 100 ) {
-  console.log("that's a big number");
-}
+// var inputNumber = 240;
+// if (inputNumber > 100 ) {
+//   console.log("that's a big number");
+// }
 
-// # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+// // # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
-var numberOne = 37; 
-var numberTwo = 73; 
+// var numberOne = 37; 
+// var numberTwo = 73; 
 
-console.log(numberOne + numberTwo); 
+// console.log(numberOne + numberTwo); 
 
-// # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+// // # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
-var inputWord2 = "break"; 
-console.log(inputWord2.split("").reverse().join(""));
+// var inputWord2 = "break"; 
+// console.log(inputWord2.split("").reverse().join(""));
 
 
-// # 5. Write a program that asks the user to enter a number, then prints the number times 10.
+// // # 5. Write a program that asks the user to enter a number, then prints the number times 10.
 
-var inputNumber2 = 10; 
-console.log(inputNumber2 * 10);
+// var inputNumber2 = 10; 
+// console.log(inputNumber2 * 10);
 
-// # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
-var inputWord3 = window.prompt("enter a word: "); 
-var inputWord4 = window.prompt("enter another word: "); 
-console.log(inputWord3.toUpperCase() + " " + inputWord4.toUpperCase()); 
+// // # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+// var inputWord3 = window.prompt("enter a word: "); 
+// var inputWord4 = window.prompt("enter another word: "); 
+// console.log(inputWord3.toUpperCase() + " " + inputWord4.toUpperCase()); 
 
-// # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+// // # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
 
-var inputWord5 = window.prompt("please enter a word");
-console.log(inputWord5.length);
+// var inputWord5 = window.prompt("please enter a word");
+// console.log(inputWord5.length);
 
-// # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
-var inputNumber3 = window.prompt("enter a number"); 
-if (parseInt(inputNumber3) < 0 ) {
-  console.log("that's a negative number"); 
-}
+// // # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+// var inputNumber3 = window.prompt("enter a number"); 
+// if (parseInt(inputNumber3) < 0 ) {
+//   console.log("that's a negative number"); 
+// }
 
-// # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
-var inputNumber4 = window.prompt("enter a number");
-var inputNumber5 = window.prompt("enter a number"); 
-console.log(parseInt(inputNumber4) * parseInt(inputNumber5)); 
+// // # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+// var inputNumber4 = window.prompt("enter a number");
+// var inputNumber5 = window.prompt("enter a number"); 
+// console.log(parseInt(inputNumber4) * parseInt(inputNumber5)); 
 
-// # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+// // # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
 
-var inputWord6 = window.prompt("enter a word"); 
-if (inputWord6.length > 5) {
-  console.log("that's a long word"); 
-}
+// var inputWord6 = window.prompt("enter a word"); 
+// if (inputWord6.length > 5) {
+//   console.log("that's a long word"); 
+// }
