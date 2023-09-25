15c0c779e404b74c6b299c417bb8133bd9ab0f29
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-25 13:06:45 -0400

    array map js practice

--- array_map_practice.js
+++ array_map_practice.js
@@ -136,16 +136,40 @@ console.log(cap);
 // #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
 
+var c = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]; 
+var first = []; 
 
+c.forEach(function (person) {
+  first.push(person.name);
+});
+console.log(first);
 
 // #  4. Start with an array of numbers and create a new array with each number plus 7.
 // #     For example, [1, 2, 3] becomes [8, 9, 10].
 
+var d = [1, 2, 3]; 
+var plus7 = []; 
+
+d.forEach(function (num) {
+  plus7.push(num + 7); 
+});
+console.log(plus7);
+
 // #  5. Start with an array of strings and create a new array with each string's length.
 // #     For example, ["hello", "goodbye"] becomes [5, 7].
 
+var e = ["hello", "goodbye"]; 
+var lengths = []; 
+
+e.forEach(function (word) {
+  lengths.push(word.length); 
+});
+console.log(lengths);
+
 // #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 // #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+
+
 
 // #  7. Start with an array of numbers and create a new array with each number divided by 2.
 // #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
