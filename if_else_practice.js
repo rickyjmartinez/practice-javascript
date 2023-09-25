04b980be4874dccb18c1d7919633bae87b8a5a62
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-25 00:36:05 -0400

    if_else js practice

--- if_else_practice.js
+++ if_else_practice.js
@@ -199,9 +199,49 @@ if (g > 10 || h > 10) {
 }
 // # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 
+var i = -1; 
+
+if (i < 0) {
+  console.log(1776); 
+} else {
+  console.log(1979); 
+}
 
 // # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
 
+var j = 100; 
+
+if (j === 100) {
+  console.log(100); 
+} else if (j === 99) {
+  console.log(99); 
+} else {
+  console.log(0);
+}
+
 // # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
 
+var k = -1; 
+var l = 1; 
+
+if (k < 0 && l > 0) {
+  console.log(1); 
+} else {
+  console.log(0); 
+}
+
 // # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+var m = 79; 
+
+if (m > 80) {
+  console.log(5); 
+} else if (m > 60) {
+  console.log(4); 
+} else if (m > 40) {
+  console.log(3); 
+} else if (m > 20) {
+  console.log(2); 
+} else {
+  console.log(1);
+}
