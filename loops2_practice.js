2f6bb715de9663d707dff23107c561ecd02edf6d
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-25 01:06:02 -0400

    loops2 js practice

--- loops2_practice.js
+++ loops2_practice.js
@@ -72,3 +72,87 @@
 // for (var i = 2; i <= 40; i += 2) {
 //   console.log(i);
 // }
+
+// # 1. Write a while loop to print the numbers 1 through 10.
+
+var a = 1; 
+
+while (a <= 10) {
+  console.log(a); 
+  a ++; 
+}
+
+// # 2. Write a while loop that prints the word "hello" 5 times.
+
+var b = 1; 
+
+while (b <= 5) {
+  console.log("hello");
+  b ++; 
+} 
+
+// # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+// while (true) {
+//   var input = window.prompt("please enter a word"); 
+//   if (input === "stop") {
+//     break; 
+//   }
+// }
+
+// # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+var c = 0; 
+while (c <= 100) {
+  console.log(c); 
+  c += 5; 
+}
+
+// # 5. Write a while loop that prints the number 9000 ten times.
+
+var d = 0;
+
+while (d <= 10) {
+  console.log(9000);
+  d ++;
+}
+
+// # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+// while (true) {
+//   var input = window.prompt("enter a number"); 
+//   if (input > 10) {
+//     break; 
+//   }
+// }
+
+// # 7. Write a while loop that prints the numbers 50 to 70.
+
+var e = 50; 
+while (e <= 70) {
+  console.log(e); 
+  e++;
+}
+
+// # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+var f = 0; 
+while (f <= 144) {
+  console.log("around the world"); 
+  f++;
+}
+// # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+// while (true) { 
+//   var input = window.prompt("please enter word"); 
+//   if (input.length > 5) {
+//     break; 
+//   }
+// }
+
+// # 10. Write a while loop that prints the even numbers from 2 to 40.
+
+var g = 2; 
+while (g <= 40) {
+  console.log(g);
+  g += 2; 
+}
