f5638416529709becfdae0fb764ad8c21a0356fd
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-25 14:26:26 -0400

    array select js practice

--- arrays_select_practice.js
+++ arrays_select_practice.js
@@ -174,22 +174,94 @@ console.log(greater5);
 // #  4. Start with an array of numbers and create a new array with only the even numbers.
 // #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
+var d = [2, 4, 5, 1, 8, 9, 7]; 
+var even = []; 
+
+d.forEach(function (num) {
+  if (num % 2 === 0) {
+    even.push(num);
+  }
+});
+console.log(even); 
 
 
 // #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 // #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
+var e = ["a", "man", "a", "plan", "a", "canal", "panama"]; 
+var less4 = []; 
+
+e.forEach(function (word) {
+  if (word.length < 4) {
+    less4.push(word);
+  }
+});
+ 
+console.log(less4);
+
 // #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+
+var f =  [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]; 
+
+var less6 = []; 
+
+f.forEach(function (item) {
+  if (item.name.length < 6) {
+    less6.push(item);
+  }
+});
+console.log(less6);
 
 // #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 // #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
 
+var g = [8, 23, 0, 44, 1980, 3]; 
+
+var less10 = []; 
+
+g.forEach(function (num) {
+  if (num < 10) {
+    less10.push(num); 
+  }
+});
+console.log(less10); 
+
 // #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 // #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+
+var h = ["big", "little", "good", "bad"]; 
+var notB = []; 
+
+h.forEach(function (word) {
+  if (word[0] !== "b") {
+    notB.push(word); 
+  }
+});
+console.log(notB); 
 
 // #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
+var i = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}];
+var costLess10 = []; 
+
+i.forEach(function (item) {
+  if (item.price < 10 ) {
+    costLess10.push(item); 
+  }
+});
+console.log(costLess10); 
+
 // # 10. Start with an array of numbers and create a new array with only the odd numbers.
 // #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+var j = [2, 4, 5, 1, 8, 9, 7]; 
+var odd = []; 
+
+j.forEach(function (num) {
+  if (num % 2 === 1) {
+    odd.push(num);
+  }
+});
+console.log(odd); 
