1a6e7bedf417ed9ef79e045e91b0aaf3cf6836c0
ricky martinez <rickyjmartinez2@gmail.com>
2023-10-30 14:56:46 -0400

    if else js practice

--- if_else_practice.js
+++ if_else_practice.js
@@ -278,16 +278,74 @@ if (c < 10 && d < 10) {
 }
 
 // # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+let e = 9001; 
+if (e > 9000) {
+  console.log("IT'S OVER 9000"); 
+} else {
+  console.log(-1);
+}
 
 // # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+let f = 5;
+if (f < 10) { 
+  console.log(9);
+} else if (f < 20) {
+  console.log(19);
+} else if (f < 30) {
+  console.log(29);
+} else {
+  console.log(-1);
+}
 
 // # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+let g = 11;
+let h = 13; 
+if (g > 10 && h > 10) {
+  console.log(100);
+} else {
+  console.log(-100);
+}
 
 // # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+let i = -1; 
+if (i < 0) {
+  console.log(1776); 
+} else {
+  console.log(1979);
+}
 
 // # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+let j = 100; 
+if (j === 100) {
+  console.log(100); 
+} else if (j === 99) {
+  console.log(99); 
+} else {
+  console.log(0);
+}
 
 // # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+let k = -1; 
+let l = 1; 
+
+if (k < 0 && l > 0) {
+  console.log(1);
+} else {
+  console.log(0); 
+}
 
 // # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+let m = 61; 
+if (m > 80) {
+  console.log(5); 
+} else if (m > 60) {
+  console.log(4);
+} else if (m > 40) {
+  console.log(3); 
+} else if (m > 20) {
+  console.log(2);
+} else {
+  console.log(1);
+}
 
