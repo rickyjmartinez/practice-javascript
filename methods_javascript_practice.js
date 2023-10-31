f02ecf30567c4e37e14b7b16ae773f7c466fbd40
ricky martinez <rickyjmartinez2@gmail.com>
2023-10-31 15:23:58 -0400

    methods js practice

--- methods_javascript_practice.js
+++ methods_javascript_practice.js
@@ -55,3 +55,84 @@
 // if (inputWord6.length > 5) {
 //   console.log("that's a long word"); 
 // }
+
+
+// # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+// let a = window.prompt("please enter a word");
+let a = "word"; 
+console.log(a.toUpperCase());
+
+
+// # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+// let b = window.prompt("please enter a number"); 
+let b = 101; 
+if (b > 100) {
+  console.log("that's a big number"); 
+}
+
+// # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+// let c = window.prompt("please enter a number"); 
+// let d = window.prompt("please enter another number"); 
+
+// console.log(parseInt(c) + parseInt(d)); 
+
+let c = 1; 
+let d = 2; 
+
+console.log(c + d); 
+
+// # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+// let e = window.prompt("please enter a word"); 
+let e = "word"; 
+console.log(e.split("").reverse().join(""));
+
+// # 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+// let f = window.prompt("please enter a number"); 
+// console.log(parseInt(f) * 10); 
+let f = 10;
+console.log(f * 10); 
+
+// # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+// let g = window.prompt("please enter a word"); 
+// let h = window.prompt("please enter another word"); 
+let g = "another"; 
+let h = "word"; 
+
+console.log(g.toUpperCase() + h.toUpperCase()); 
+
+
+// # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+// let i = window.prompt("please enter a word"); 
+let i = "word"; 
+console.log(i.length); 
+
+// # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+// let j = window.prompt("please enter a number"); 
+// if (parseInt(j) < 0) {
+//   console.log("that's a negative number"); 
+// }
+
+let j = -1; 
+if (j < 0) {
+  console.log("that's a negative number"); 
+}
+
+// # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+// let k = window.prompt("please enter a number"); 
+// let l = window.prompt("please enter another number"); 
+// console.log(parseInt(k) * parseInt(l)); 
+
+let k = 10; 
+let l = 150; 
+console.log(k * l); 
+
+// # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+let m = window.prompt("please enter word"); 
+if (m.length > 5) {
+  console.log("that's a long word"); 
+}
