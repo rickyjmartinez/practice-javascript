f1914aef4e4c6526c404bfe119bbe9a54da8469a
ricky martinez <rickyjmartinez2@gmail.com>
2024-01-02 13:04:35 -0500

    string javascript practice

--- strings_practice.js
+++ strings_practice.js
@@ -1,136 +1,191 @@
+// // // # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+// // var first = "ricky";
+// // var last = "martinez"; 
+// // console.log(first + " " + last); 
+
+// // // # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+// // var first = "ricky"; 
+// // var last = "martinez"; 
+// // console.log(`${first} ${last}`);
+
+// // // # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+// // var input = "marco"; 
+// // if (input === "marco") {
+// //   console.log("polo");
+// // }
+
+// // // # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+// // var color1 = "purple"; 
+// // var color2 = "red"; 
+// // var color3 = "green"; 
+// // console.log(color1 + ", " + color2 + ", " + "and " + color3 + " are pretty."); 
+
+// // // # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+// // var color1 = "purple"; 
+// // var color2 = "green"; 
+// // var color3 = "blue"; 
+// // console.log(`${color1}, ${color2}, and ${color3} are pretty.`); 
+
+// // // # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+// // var nameInput = "ricky"; 
+// // if (nameInput !== "santa") {
+// //   console.log("you're not Santa."); 
+// // }
+
+// // // # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+// // var title = "the hobbit"; 
+// // var author = "jrr tolkien"; 
+// // console.log(title + " was written by " + author + "."); 
+
+// // // # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+ 
+// // var title = "the hobbit"; 
+// // var author = "jrr tolkien"; 
+// // console.log(`${title} was written by ${author}.`);
+
+// // // # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+// // var inputPassword = "Joshua"; 
+// // if (inputPassword === "Joshua") {
+// //   console.log("Shall we play a game?"); 
+// // } else {
+// //   console.log("Access denied");
+// // }
+
+// // // # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+// // var city1 = "chicago"; 
+// // var city2 = "indianapolis"; 
+// // var city3 = "denver"; 
+// // console.log(city1 + ", " + city2 + ", and " + city3 + " are in the US."); 
+
+
 // // # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
-// var first = "ricky";
-// var last = "martinez"; 
-// console.log(first + " " + last); 
+// let a = "ricky"; 
+// let b = "martinez"; 
+
+// console.log(a + " " + b);
 
 // // # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
-
-// var first = "ricky"; 
-// var last = "martinez"; 
-// console.log(`${first} ${last}`);
+// let c = "ricky";
+// let d = "martinez"; 
+// console.log(`${c} ${d}`);
 
 // // # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
-
-// var input = "marco"; 
-// if (input === "marco") {
-//   console.log("polo");
-// }
+// // let input = window.prompt("enter a word");
+// // if (input === "marco") {
+// //   console.log("polo");
+// // }
 
 // // # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+// let e = "red"; 
+// let f = "blue"; 
+// let g = "green"; 
 
-// var color1 = "purple"; 
-// var color2 = "red"; 
-// var color3 = "green"; 
-// console.log(color1 + ", " + color2 + ", " + "and " + color3 + " are pretty."); 
+// console.log(e + " " + f + " " + g);
 
 // // # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+// let h = "red"; 
+// let i = "blue"; 
+// let j = "green"; 
 
-// var color1 = "purple"; 
-// var color2 = "green"; 
-// var color3 = "blue"; 
-// console.log(`${color1}, ${color2}, and ${color3} are pretty.`); 
+// console.log(`${h} ${i} ${j}`);
 
 // // # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
-
-// var nameInput = "ricky"; 
-// if (nameInput !== "santa") {
-//   console.log("you're not Santa."); 
-// }
+// // let santa = window.prompt("please enter a name"); 
+// // if (santa !== "santa") {
+// //   console.log("you're not santa"); 
+// // }
 
 // // # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
 
-// var title = "the hobbit"; 
-// var author = "jrr tolkien"; 
-// console.log(title + " was written by " + author + "."); 
+// let k = "the hobbit";
+// let l = "jrr tolkien";
+
+// console.log("the book " + k + " " + "was written by " + l);
 
 // // # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
- 
-// var title = "the hobbit"; 
-// var author = "jrr tolkien"; 
-// console.log(`${title} was written by ${author}.`);
+
+// let m = "the hobbit";
+// let n = "jrr tolkien"; 
+
+// console.log(`the book ${m} was written by ${n}`); 
 
 // // # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
-// var inputPassword = "Joshua"; 
-// if (inputPassword === "Joshua") {
-//   console.log("Shall we play a game?"); 
-// } else {
-//   console.log("Access denied");
-// }
+// // let pass = window.prompt("please enter a word"); 
+// // if (pass === "Joshua") {
+// //   console.log("shall we play a gam?"); 
+// // } else {
+// //   console.log("access denied");
+// // }
 
 // // # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
 
-// var city1 = "chicago"; 
-// var city2 = "indianapolis"; 
-// var city3 = "denver"; 
-// console.log(city1 + ", " + city2 + ", and " + city3 + " are in the US."); 
+// let o = "chicago"; 
+// let p = "mexico city";
+// let q = "paris"; 
+// console.log("three cities in the world are " + o + " " + p + " " + q); 
 
 
 // # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
-let a = "ricky"; 
+let a = "ricky";
 let b = "martinez"; 
 
-console.log(a + " " + b);
+console.log(a + " " + b); 
 
 // # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
-let c = "ricky";
+
+let c = "ricky"; 
 let d = "martinez"; 
-console.log(`${c} ${d}`);
+
+console.log(`${a} ${b}`); 
 
 // # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
-// let input = window.prompt("enter a word");
-// if (input === "marco") {
+
+// let e = window.prompt("please enter a word"); 
+
+// if (e === "marco") { 
 //   console.log("polo");
 // }
 
 // # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
-let e = "red"; 
-let f = "blue"; 
-let g = "green"; 
 
-console.log(e + " " + f + " " + g);
+let f = "red"; 
+let g = "blue"; 
+let h = "green"; 
+
+console.log(f + g + h); 
 
 // # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
-let h = "red"; 
-let i = "blue"; 
-let j = "green"; 
 
-console.log(`${h} ${i} ${j}`);
+let i = "red"; 
+let j = "blue"; 
+let k = "green"; 
+
+console.log(`${i} ${j} ${k}`); 
 
 // # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
-// let santa = window.prompt("please enter a name"); 
-// if (santa !== "santa") {
-//   console.log("you're not santa"); 
+
+// let l = window.prompt("please enter a name"); 
+// if (l.toLowerCase !== "santa") { 
+//   console.log("You're not Santa"); 
 // }
+
 
 // # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
 
-let k = "the hobbit";
-let l = "jrr tolkien";
-
-console.log("the book " + k + " " + "was written by " + l);
-
 // # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
-
-let m = "the hobbit";
-let n = "jrr tolkien"; 
-
-console.log(`the book ${m} was written by ${n}`); 
 
 // # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
-// let pass = window.prompt("please enter a word"); 
-// if (pass === "Joshua") {
-//   console.log("shall we play a gam?"); 
-// } else {
-//   console.log("access denied");
-// }
-
 // # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
-
-let o = "chicago"; 
-let p = "mexico city";
-let q = "paris"; 
-console.log("three cities in the world are " + o + " " + p + " " + q); 
